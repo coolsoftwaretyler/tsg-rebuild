21bfde695fa80867475d05dbf36ad6285eeee2be
Tyler <tyler@ogdenstudios.xyz>
2018-09-07 15:06:21 -0600

    add working demo counters

--- static/js/milestone.js
+++ static/js/milestone.js
@@ -1,33 +1,25 @@
-/* Milestone Number Shortcode */
-/* -------------------------------------------------------------------- */
-mk_milestone();
-function mk_milestone() {
-  console.log("running milestone script");
-  "use strict";
-  $('.mk-milestone').each(function () {
-    var $this = $(this),
-      stop_number = $this.find('.milestone-number').attr('data-stop'),
-      animation_speed = parseInt($this.find('.milestone-number').attr('data-speed'));
-    var build = function() {
-      if (!$this.hasClass('scroll-animated')) {
-        $this.addClass('scroll-animated');
-        $({
-          countNum: $this.find('.milestone-number').text()
-        }).animate({
-          countNum: stop_number
-        }, {
-          duration: animation_speed,
-          easing: 'linear',
-          step: function () {
-            $this.find('.milestone-number').text(Math.floor(this.countNum));
-          },
-          complete: function () {
-            $this.find('.milestone-number').text(this.countNum);
-          }
-        });
-      }
-    };
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 8000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
 
   });
 
-}
+
+
+});
